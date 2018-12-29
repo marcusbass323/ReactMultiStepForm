1e1c7b4e285ffb252d05ccab0d4a1ca81053bf66
marcusbass323 <marcus@bassconsultantgroup.com>
2018-12-28 23:28:42 -0800

    Success page done

--- src/components/UserForm.js
+++ src/components/UserForm.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import FormUserDetails from './FormUserDetails';
 import FormPersonalDetails from './FormPersonalDetails';
-import Confirm from './Confirm'
+import Confirm from './Confirm';
+import Success from './Success';
 export class UserForm extends Component {
 
     state = {
@@ -66,7 +67,7 @@ export class UserForm extends Component {
                 />
             );
             case 4:
-                return <h1>Success</h1>;    
+                return <Success />;    
         }
   }
 }
